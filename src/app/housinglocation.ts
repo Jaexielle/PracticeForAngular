63bf5ee6fdaae60177135d8df69faeb586b98764
unknown <jeselle184@gmail.com>
2024-03-31 18:58:12 +0800

    First Commit: Codes are from Angular Practice Project will use it as reference for future projs maybe

---
+++ src/app/housinglocation.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+export interface HousingLocation {
+    id: number;
+    name: string;
+    city: string;
+    state: string;
+    photo: string;
+    availableUnits: number;
+    wifi: boolean;
+    laundry: boolean;
+  }
